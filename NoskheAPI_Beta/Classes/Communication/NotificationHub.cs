9c3c8a6feb0f13d665fa07d43c3e2e42d727947b
MH.Movasaghi <mmghho77@gmail.com>
2018-11-28 11:05:09 +0330

    Processing on List With Signal R

--- NoskheAPI_Beta/Classes/Communication/NotificationHub.cs
+++ NoskheAPI_Beta/Classes/Communication/NotificationHub.cs
@@ -1,23 +1,64 @@
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Generic;
 
 namespace NoskheAPI_Beta.Classes.Communication
 {
+    /*
+     * Template For Sending Noskhe To Pharmacy : 
+     * 
+     *  {
+     *      Url1 = "www.Sample.com",
+     *      Url2 = "www.Sample.com",
+     *      Url3 = "www.Sample.com",
+     *      
+     *  }
+     */
     public class NotificationHub : Hub
     {
         public async Task Initialize(string identifier)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, identifier);
-            await Clients.Group(identifier).SendAsync("HandleNotification", new { Url1 = "www.people.com", Url2 = "www.Picture.com", Url3 = "www.p.com" });
+            List<NoskheAtFirst> A = new List<NoskheAtFirst>()
+            {
+                new NoskheAtFirst()
+                {
+                    Picture_Urls = new List<string>()
+                    {
+                        "www.google.com",
+                        "www.pashm.com",
+                    },
+                    WithOutNoskhe_List = new List<WithOutNoskhe>()
+                    {
+                        new WithOutNoskhe()
+                        {
+                            Number = 10,
+                            Name = "Pashm"
+                        },
+                        new WithOutNoskhe()
+                        {
+                            Number = 20,
+                            Name = "Salam"
+                        },
+                    },
+                },
+            };
+            await Clients.Group(identifier).SendAsync("HandleNotification", A);
         }
         public async Task SendMessage(string identifier, object arg1)
         {
             await Clients.Group(identifier).SendAsync("HandleNotification", arg1);
         }
     }
-    public class WithOutNoskheFist
+    public class NoskheAtFirst
+    {
+        public List<string> Picture_Urls { set; get; }
+        public List<WithOutNoskhe> WithOutNoskhe_List { set; get; }
+    }
+    public class WithOutNoskhe
     {
         public int Number { get; set; }
-        public string List { get; set; }
+        public string Name { get; set; }
     }
+
 }

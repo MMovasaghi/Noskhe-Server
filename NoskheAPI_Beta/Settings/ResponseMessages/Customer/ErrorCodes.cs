f937d8fc23cf9e08c6bb7eb1319e9e0d0272fce4
AmirMohammad Biuki <dark.c343@gmail.com>
2018-12-11 23:52:04 +0330

    Error codes fixed

--- NoskheAPI_Beta/Settings/ResponseMessages/Customer/ErrorCodes.cs
+++ NoskheAPI_Beta/Settings/ResponseMessages/Customer/ErrorCodes.cs
@@ -9,18 +9,17 @@ namespace NoskheAPI_Beta.Settings.ResponseMessages.Customer
         public static string InvalidCosmeticIDFoundExceptionMsg = "CEC4";
         public static string InvalidMedicineIDFoundExceptionMsg = "CEC5";
         public static string NoCosmeticsAvailabeExceptionMsg = "CEC6";
-        public static string NoCosmeticsMatchedByUSCIExcpetionMsg = "CEC7";
-        // public static string NoCustomersFoundExceptionMsg = "CEC8";
-        public static string NoCustomersMatchedByPhoneExceptionMsg = "CEC9";
-        public static string NoMedicinesAvailabeExceptionMsg = "CEC10";
-        public static string NoMedicinesMatchedByUSCIExcpetionMsg = "CEC11";
-        public static string NoOrdersFoundExceptionMsg = "CEC12";
-        public static string NoOrdersMatchedByUOIExceptionMsg = "CEC13";
-        public static string NoShoppingCartsFoundExceptionMsg = "CEC14";
-        public static string PaymentGatewayFailureExceptionMsg = "CEC15";
-        public static string LoginVerificationFailedExceptionMsg = "CEC16";
-        public static string SmsVerificationExpiredExceptionMsg = "CEC17";
-        public static string SmsVerificationFailedExceptionMsg = "CEC18";
+        public static string NoCosmeticsInTheShoppingCartExceptionMsg = "CEC7";
+        public static string NoCustomersMatchedByPhoneExceptionMsg = "CEC8";
+        public static string NoMedicinesAvailabeExceptionMsg = "CEC9";
+        public static string NoMedicinesInTheShoppingCartExceptionMsg = "CEC10";
+        public static string NoOrdersFoundExceptionMsg = "CEC11";
+        public static string NoOrdersMatchedByIdException = "CEC12";
+        public static string NoShoppingCartsFoundExceptionMsg = "CEC13";
+        public static string PaymentGatewayFailureExceptionMsg = "CEC14";
+        public static string LoginVerificationFailedExceptionMsg = "CEC15";
+        public static string SmsVerificationExpiredExceptionMsg = "CEC16";
+        public static string SmsVerificationFailedExceptionMsg = "CEC17";
         // C# Standard Exception Error Code Messages
         public static string SecurityTokenExpiredExceptionMsg = "CEC0";
     }

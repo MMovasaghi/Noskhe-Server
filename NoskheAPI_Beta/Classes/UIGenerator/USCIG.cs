d2c5332f83c8e376bf5698486f2ef3694b7a27c9
MH.Movasaghi <mmghho77@gmail.com>
2019-02-20 17:09:42 +0330

    USCIG

--- NoskheAPI_Beta/Classes/UIGenerator/USCIG.cs
+++ NoskheAPI_Beta/Classes/UIGenerator/USCIG.cs
@@ -6,18 +6,34 @@ namespace NoskheAPI_Beta.Classes.UIGenerator
 {
     public static class USCIG // unique shopping cart identifier generator
     {
+        /*
+         *    N-{0}{1}{2}{3}{4}
+         *    {0} : version
+         *    {1} : Year : { EX: 97 }
+         *    {2} : Month
+         *    {3} : Day
+         *    {4} : 0001 : 10^4 state
+         */
+        private static int Version = 1;
         public static string Generate()
         {
-            NoskheContext db  = new NoskheContext();
-            if(db.ShoppingCarts.Count() != 0)
+            try
             {
-                string usciString = db.ShoppingCarts.Last().USCI;
-                int usciInt = int.Parse(usciString.Substring(2,6));
-                if(db.ShoppingCarts.Where(s => s.USCI == "").FirstOrDefault() == null)
-                    return "SH" + usciInt;
-                // else?!
+                NoskheContext db = new NoskheContext();
+                if (db.ShoppingCarts.Count() != 0)
+                {
+                    long uoiString = db.ShoppingCarts.Last().ShoppingCartId;
+                    return string.Format("N-{0}{1}{2}{3}{4}", Version, DateTime.Now.Year.ToString(), DateTime.Now.Month.ToString(), DateTime.Now.Day.ToString(), uoiString.ToString() + 1000);
+                }
+                else
+                {
+                    throw new Exception();
+                }
             }
-            return "SH100000";
+            catch (Exception)
+            {
+                throw new Exception();
+            }
         }
     }
 }
